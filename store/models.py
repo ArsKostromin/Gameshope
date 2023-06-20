558dd3bd4817f81685500ae347522a3e7f9b6510
SKYFALL <arsyakost@gmail.com>
2023-06-20 22:01:02 +0500

    gamedetail  url - games<index>

--- store/models.py
+++ store/models.py
@@ -34,7 +34,7 @@ class St(models.Model):
         """
         Returns the url to access a particular book instance.
         """
-        return reverse('st-detail', args={ self.slug})
+        return reverse('st-detail', args=[str(self.id)])
 
     def __str__(self):
         """
--- store/urls.py
+++ store/urls.py
@@ -10,7 +10,7 @@ from django.conf.urls.static import static
 urlpatterns = [   
     path('<int:genre_id>', by_genre, name = 'by_genre'),
     path('', StoreListView.as_view(), name = 'index'),
-    path('<slug>', StoreDetailView.as_view(), name='st-detail'),
+    path('games<pk>', StoreDetailView.as_view(), name='st-detail'),
     path('<slug>', PublisherDetailView.as_view(), name='publisher-detail'),
     path('mygames/', LoanedStsByUserListView.as_view(), name='my-borrowed'),
     path('signup/', SignUp.as_view(), name="signup")
