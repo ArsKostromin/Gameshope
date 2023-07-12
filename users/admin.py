19e1129aff66df8b6e83f8ec66baaf1d4537dc70
SKYFALL <arsyakost@gmail.com>
2023-07-12 21:02:49 +0500

    prpfile

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Profile
 
 # Register your models here.
+
+class ProfileAdmin (admin.ModelAdmin):
+    list_display = ('name', 'id')
+
+
 admin.site.register(Profile)
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,25 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
  
 urlpatterns = [
     path('', views.profiles, name="profiles"),
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
+
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+'''    
+    path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),  
+    path('account/', views.userAccount, name="account"),
+    path('edit-account/', views.editAccount, name="edit-account"),
+    
+    '''
